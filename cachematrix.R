e59c329061d3b2f79fa5a41f896c2d55413367c6
Lalit Umbarkar <lalit.umbarkar9@gmail.com>
2015-01-18 12:21:29 +0530

    Comments and cacheSolve Function
    Added comments to the file and completed the function cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This file is used to calculate the inverse of the matrix 
+## efficiently for a large data.
+## It uses the concept of caching the inverse of the matrix 
+## already calculated and displayes whenever needed.
+
+
+## Function is used to cache inverse of matrix is until now 
+## it wasn't calculated, or if was calculated earlier then it
+## extracted from the cache memory.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    
 }
 
 
-## Write a short comment describing this function
+## Function is used to calculate the Inverse of given Matrix
+## Function is called only when there is a need for calculating
+## the invere of the matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+    ## Returns a matrix that is the inverse of 'x'
+    return (solve(x))
 }
